76c17f981c7aa812d061f60ecd4e070e934a1d0d
Mediha Zukic <mzukic@eduroam-int-dhcp-97-206-136.ucl.ac.uk>
2024-08-15 15:08:31 +0100

    Wrapper class for passing double as a reference

---
+++ helpers/DoubleWrapper.m
@@ -0,0 +1,18 @@
+% A wrapper class to enable passing double by reference
+classdef DoubleWrapper < handle
+    properties
+        Val
+    end
+    
+    methods
+        function obj = DoubleWrapper(val)
+            if nargin > 0
+                obj.Val = val;
+            end
+        end
+        
+        function setValue(obj, newValue)
+            obj.Val = newValue;
+        end
+    end
+end
