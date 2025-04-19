9a96d9dbd73e3fefd1554d992e54b4432ec54c10
Mediha Zukic <mzukic@Medihas-MacBook-Pro.local>
2025-04-19 15:46:28 +0200

    Refactor sgfa_profiler

--- experiments/gfa/sgfa_profiler.m
+++ experiments/gfa/sgfa_profiler.m
@@ -19,47 +19,26 @@ settings.DEBUG = false;
 
 
 %% Generate data and train the model
-data = generateTwoViews();
+data = Datasets.generateSyntheticGFAData(2);
 
-X1 = data.X_tr{1}; % [N x D1];
-X2 = data.X_tr{2}; % [N x D2]
+X1 = data.X_train{1}; % [D1 x N]
+X2 = data.X_train{2}; % [D2 x N]
 
 profile on;
 
 K = 10;
-sgfaModel = SGFA({X1', X2'}, K);
+sgfaModel = SGFA({X1, X2}, K);
 [elboVals, convIt] = sgfaModel.fit(10);
 
 
 profile off;
 profile viewer;
 
-%%
-%                             Z,         figureTitle,        figName,      folderName)
-Visualization.plotLatentFactors(data.Z, 'True latent factors', 'trueLatentFactors', figsSubfolder);
-Visualization.plotLatentFactors(sgfaModel.Z.E', 'Latent factors', 'latentFactors', figsSubfolder);
-% TODO: reorder these!!! no '
+%% Visualize true and inferred latent factors
+Visualization.plotLatentFactors(data.Z, 'True latent factors', '', mfilename);
+Visualization.plotLatentFactors(sgfaModel.Z.E, 'Inferred latent factors', '', mfilename);
+
 
 %% Visualize loadings and alpha
-totalD = sum(sgfaModel.D); % Total number of dimensions
-
-trueW = zeros(totalD, data.trueK); % True K
-estW = zeros(totalD, sgfaModel.K.Val);
-estAlpha = zeros(sgfaModel.K.Val, sgfaModel.M);
-
-d = 0;
-for m = 1:sgfaModel.M
-    Dm = sgfaModel.views(m).D;
-    trueW(d + 1 : d + Dm, :) = data.W{m};
-    estW(d + 1 : d + Dm, :) = sgfaModel.views(m).W.E;
-    d = d + Dm;
-
-    estAlpha(:, m) = sgfaModel.views(m).alpha.E;
-end
-
-%                            W,     dimList,   figTitle, figName, subfolderName
-Visualization.plotLoadings(trueW, sgfaModel.D, 'True $\mathbf{W}$', 'trueW', figsSubfolder);
-Visualization.plotLoadings(estW, sgfaModel.D, 'Estimated $\mathbf{W}$', 'estimatedW', figsSubfolder);
-
-% alpha
-Visualization.plotHintonDiagrams({-data.alpha', -estAlpha}, 'Lala');
+Visualization.plotFactorLoadingsAndAlpha(data.W, data.D, data.alpha, 'bottom', '', 2.5, 'True $\mathbf{W}^\top$ and \boldmath{$\alpha$}$^\top$');
+Visualization.plotFactorLoadingsAndAlpha(sgfaModel.W, sgfaModel.D, sgfaModel.alpha, 'bottom', '', 2.5, 'Inferred $\mathbf{W}^\top$ and \boldmath{$\alpha$}$^\top$');
